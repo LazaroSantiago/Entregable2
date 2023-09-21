136b217297402272679e2a514470a82d0f2d89ad
Santiago Lázaro <santi.lazaro@gmail.com>
2023-09-21 00:08:32 -0300

    Update comments

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,8 +8,8 @@ public class Main {
 
 //        TODO:Hay problemas de delegacion que resolver... el JPA Controller esta bien? Quien se encarga de las queries?
 //        Al parecer hay que hacer repositories en vez de tener la carpeta CRUD. Un repositorio por objeto y ahi se encajan las queries
-//        Primero hay que hacer lo de abajo, eso es el 4. Despues se pueden hacer repositorios, DTOs, hacer fetch lazy las relaciones etc.
-
+//        Primero hay que hacer lo de abajo, eso es el 4. Despues se pueden hacer repositorios, DTOs, factory, hacer fetch lazy las relaciones etc.
+//        TODO:Hacer el diagramita de clases (DER). Si, esta en las cosas que evaluan. No hago las reglas xd
 //        TODO:e) recuperar todos los estudiantes, en base a su género.
 //        TODO:g) recuperar los estudiantes de una determinada carrera, filtrado por ciudad de residencia
 //        TODO: Generar un reporte de las carreras, que para cada carrera incluya información de los
